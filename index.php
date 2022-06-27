b31e7ff0fb8587a98681947a8b6efcf243b7e470
algokelvin-373 <kelvinht373@gmail.com>
2022-06-27 17:48:06 +0700

    Test code PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>AlgoKelvin Testing</title>
+        <link rel="shortcut icon" href="/algokelvin_373.github.io/image/algokelvin.png">
+    </head>
+    <body>
+        <p>
+            AlgoKelvin is an Online Class and Channel Creator to enhance the development of "MAKE SOFTWARE" skills. <br>
+            Follow Algokelvin and you will be taught various methods and techniques easy and precise in making software. <br>
+            Here it is also taught to learn team collaboration which is a provision to enter the world of Technology work.
+        </p>
+        <?php
+            echo "Test PHP AlgoKelvin";
+        ?> <br>
+        <b>Comming Soon Guys</b>
+    </body>
+</html>
