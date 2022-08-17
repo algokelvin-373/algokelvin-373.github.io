d48b50e773acf783bafe709d340288c8782a123c
algokelvin-373 <kelvinht373@gmail.com>
2022-08-17 20:29:02 +0700

    [JS-Training] Create function card UI

--- kelvinht/js/demo.js
+++ kelvinht/js/demo.js
@@ -2,11 +2,15 @@ all_blogger();
 
 function all_blogger() {
 const blog = [
-"Coding Language", "Content Creator", "Software Developer"
+"Coding Language",
+"Content Creator",
+"Software Developer"
 ];
 
 const imgblog = [
-"img/img_coding_language.png", "img/img_content_creator.png", "img/img_software_developer.png"
+"img/img_coding_language.png",
+"img/img_content_creator.png",
+"img/img_software_developer.png"
 ];
 
 const decblog = [
@@ -15,21 +19,28 @@ const decblog = [
 "Topik ini membahas tentang coding secara basic (fundamental). Selain itu juga ada mindset dan mental yang harus siap dalam belajar. Ada contoh studi kasus + live coding"
 ];
 
-    let card = '<div class="row">';
-    for (let x = 0; x < blog.length; x++) {
-        card += '<div class="col-md-4">' +
-                                 '<div class="p-2 bd-highlight">' +
-                                     '<div class="card" style="width:100%">' +
-                                         '<img class="card-img-top" src="' + imgblog[x] +'" alt="Card image" style="width:100%">' +
-                                         '<div class="card-body">' +
-                                             '<h4 class="card-title">' + blog[x] +'</h4>' +
-                                             '<p class="card-text">' + decblog[x] + '</p>' +
-                                             '<a href="#" class="btn btn-primary">View More</a>' +
-                                         '</div>' +
-                                     '</div>' +
-                                 '</div>' +
-                             '</div>';
-    }
+layout_card(imgblog, blog, decblog);
+}
+
+function layout_card(img, blog, dec) {
+let card = '<div class="row">';
+for (let x = 0; x < blog.length; x++) {
+   card += ui_card(img[x], blog[x], dec[x]);
+}
 document.getElementById("demo2").innerHTML = card + '</div>';
 }
 
+function ui_card(img, blog, dec) {
+return '<div class="col-md-4">' +
+'<div class="p-2 bd-highlight">' +
+ '<div class="card" style="width:100%">' +
+ '<img class="card-img-top" src="' + img +'" alt="Card image" style="width:100%">' +
+ '<div class="card-body">' +
+ '<h4 class="card-title">' + blog +'</h4>' +
+ '<p class="card-text">' + dec + '</p>' +
+ '<a href="#" class="btn btn-primary">View More</a>' +
+ '</div>' +
+ '</div>' +
+ '</div>' +
+ '</div>';
+}
