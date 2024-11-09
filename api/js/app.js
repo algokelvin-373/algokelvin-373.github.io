128580b4e4123b08e0e7cbd066973a5e97696cdf
algokelvin-373 <kelvinht373@gmail.com>
2024-11-09 22:03:14 +0700

    [Web AlgoKelvin - API Document] Init Project API Dummy Document

---
+++ api/js/app.js
@@ -0,0 +1,35 @@
+// This file contains the main application logic
+function displayResult(data) {
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
+}
+
+// Get All User
+async function fetchUsers() {
+    try {
+        const users = await api.getUsers();
+        displayResult(users);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+    }
+}
+
+// Get User By ID
+async function fetchUserById(id) {
+    try {
+        const user = await api.getUserById(id);
+        displayResult(user)
+    } catch (error) {
+        console.error('Error fetching users by id:', error);
+    }
+}
+
+// Get All Product
+async function fetchProducts() {
+    try {
+        const products = await api.getProducts();
+        displayResult(products);
+    } catch (error) {
+        console.error('Error fetching products:', error);
+    }
+}
