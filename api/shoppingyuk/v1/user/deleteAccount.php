5dff02289d6bcb1730154fcf199bb3cd2bcba427
algokelvin-373 <kelvinht373@gmail.com>
2024-11-14 21:05:17 +0700

    [API AlgoKelvin - ShoppingYuk] API Delete Account

--- api/shoppingyuk/v1/user/deleteAccount.php
+++ api/shoppingyuk/v1/user/deleteAccount.php
@@ -2,6 +2,22 @@
 include '../db.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
+$userId = $data['userId'];
+
+function deleteProfile($pdo, $userId) {
+    $query = "DELETE FROM sy_user WHERE id = :id";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':id', $userId);
+    $stmt->execute();
+    return $stmt->rowCount() > 0;
+}
 
 $pdo = connectDB();
+
+if (deleteProfile($pdo, $userId)) {
+    resultResponse(1, 'success','Success to Delete Account');
+} else {
+    resultResponse(0, "Failed", "Failed to Delete Account");
+}
+
 ?>
