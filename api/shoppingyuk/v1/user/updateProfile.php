8054309908f7b46603c573fd5911f91b0a55708c
algokelvin-373 <kelvinht373@gmail.com>
2024-11-14 20:24:34 +0700

    [API AlgoKelvin - ShoppingYuk] API Update Profile

--- api/shoppingyuk/v1/user/updateProfile.php
+++ api/shoppingyuk/v1/user/updateProfile.php
@@ -2,6 +2,49 @@
 include '../db.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
+$path = $_SERVER['REQUEST_URI'];
+$partParts = explode('/', $path);
+
+$userId = $partParts[7];
+$nameFirst = $data['name_first'];
+$nameLast = $data['name_last'];
+$phoneNumber = $data['phone_number'];
+
+function updateProfile($pdo) {
+    global $userId, $nameFirst, $nameLast, $phoneNumber;
+
+    $query = "UPDATE sy_user SET name_first = :name_first, name_last = :name_last, phone_number = :phone_number WHERE id = :id";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':id', $userId);
+    $stmt->bindParam(':name_first', $nameFirst);
+    $stmt->bindParam(':name_last', $nameLast);
+    $stmt->bindParam(':phone_number', $phoneNumber);
+    $stmt->execute();
+    return $stmt->rowCount() > 0;
+}
+
+function getProfileUser($pdo, $userId) {
+    $query = "SELECT * FROM sy_user WHERE id = :id";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':id', $userId);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
 
 $pdo = connectDB();
+
+if (updateProfile($pdo)) {
+    $profileUser = getProfileUser($pdo,$userId);
+    if ($profileUser) {
+        $data = [
+            "name_first" => $profileUser["name_first"],
+            "name_last" => $profileUser["name_last"],
+            "username" => $profileUser["username"],
+            "phone_number" => $profileUser["phone_number"],
+        ];
+        resultResponse(1, "Success", "Success to Update Profile User", $data);
+    }
+} else {
+    resultResponse(0,'Failed', 'Failed to Update Profile User');
+}
 ?>
