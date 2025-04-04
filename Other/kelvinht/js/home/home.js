3bea446a8e82c30803281bd282e3ece14ac0bb71
algokelvin-373 <kelvinht373@gmail.com>
2025-04-04 16:55:59 +0700

    Remove and save in my local

--- Other/kelvinht/js/home/home.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-            $("#header").load("header.html");
-            $("#headerme").load("home/headerme.html");
-            $("#aboutme").load("home/aboutme.html");
-            $("#connectme").load("home/connectme.html");
-            $("#blogme").load("home/blogme.html");
-            $("#footer").load("footer.html");
-});
